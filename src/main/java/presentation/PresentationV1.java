2f5673670709944c1096efd6c9ed3f8c769f8c64
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-03-11 17:11:23 +0000

    Started BY Static Injection

--- src/main/java/presentation/PresentationV1.java
+++ src/main/java/presentation/PresentationV1.java
@@ -5,9 +5,12 @@ import metier.MetierImpl;
 
 public class PresentationV1 {
     public static void main(String[] args) {
+        /**
+         * L'injection des dépendences par instantiation statique
+         */
         DaoImpl d = new DaoImpl();
         MetierImpl metier = new MetierImpl();
-        metier.setDao(d); // Ensure the dao is set before calling calcul
+        metier.setDao(d); // Injection via le setter
         System.out.println("RESULTAT = " + metier.calcul());
     }
 }
